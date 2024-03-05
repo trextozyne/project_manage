bbf0db1270f80955cd06e18d6ee66f0949cd8024
the-rexy <daleks1987@gmail.com>
2024-03-04 18:52:53 -0600

    Completed close accordion when empty and show message

--- src/app/shared/tools/containers/task-containers/task-containers.component.ts
+++ src/app/shared/tools/containers/task-containers/task-containers.component.ts
@@ -1,7 +1,7 @@
-import {Component, computed, input} from '@angular/core';
+import {Component, ElementRef, input, ViewChild, OnChanges, ViewChildren, QueryList} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RandomBackgroundStyleDirective} from "../../../Directives/random-background-style.directive";
-import {MatExpansionModule} from "@angular/material/expansion";
+import {MatExpansionModule, MatExpansionPanel} from "@angular/material/expansion";
 import {ProgressBarComponent} from "../../progress-bar/progress-bar.component";
 import {taskPerUser} from "../../../services/projects/projects.service";
 
@@ -14,10 +14,21 @@ import {taskPerUser} from "../../../services/projects/projects.service";
 })
 export class TaskContainersComponent {
   panelOpenState:  boolean = false; //accordion
+  @ViewChild('mat_exp') matExpansionPanel!: MatExpansionPanel;
+  //@ViewChild('participants') participants!: ElementRef;
+  @ViewChildren('participants') participants!: QueryList<ElementRef>;
 
   // ==for single user  dropdown===
   userImages = input<{id: string, isAssignedToTeam: boolean, img: string}[]>();
   taskPerUser = input<taskPerUser[]>();
+
+  ngOnChanges() {
+    // Check if the element array is empty and toggle the expansion panel accordingly
+    const isEmpty = this.participants && this.participants.length === 1;
+
+    if (isEmpty)
+      this.matExpansionPanel.close();
+  }
 
   constructor() {
   }
